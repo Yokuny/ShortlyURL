93b3cdc2afeae0aeb1ae368e3084aed30331487d
yokuny <felipe.vni@hotmail.com>
2023-05-23 22:59:40 -0300

    repository pattern in process route

--- src/controllers/process.controller.js
+++ src/controllers/process.controller.js
@@ -1,15 +1,12 @@
 import process from "../repositories/processRepository.js";
 
-import db from "../database/db.database.js";
-
 const registerUrl = async (req, res) => {
   try {
     const line = await process.registerUrl(res.locals.user, req.body.url);
-    console.log("line");
-    console.log(line);
+
     res.status(201).send({
-      id: line[0].id,
-      shortUrl: line[0].shortUrl,
+      id: line.id,
+      shortUrl: line.shortUrl,
     });
   } catch (err) {
     res.status(500).send({ message: err.message });
@@ -17,17 +14,14 @@ const registerUrl = async (req, res) => {
 };
 
 const getUrl = async (req, res) => {
-  const { id } = req.params;
-  const query = `SELECT * FROM urls WHERE id = $1`;
-
   try {
-    const { rows: line } = await db.query(query, [id]);
-    if (!line.length) return res.status(404).send({ message: "URL not found" });
+    const line = await process.getUrl(req.params.id);
+    if (!line) return res.status(404).send({ message: "URL not found" });
 
     res.status(200).send({
-      id: line[0].id,
-      shortUrl: line[0].shortUrl,
-      url: line[0].url,
+      id: line.id,
+      shortUrl: line.shortUrl,
+      url: line.url,
     });
   } catch (err) {
     return res.status(500).send({ message: err.message });
@@ -35,34 +29,28 @@ const getUrl = async (req, res) => {
 };
 
 const openUrl = async (req, res) => {
-  const { shortUrl } = req.params;
-  const query = `UPDATE urls SET "visitCount" = "visitCount" + 1 WHERE "shortUrl" = $1 RETURNING url`;
-
   try {
-    const { rows: url } = await db.query(query, [shortUrl]);
-    if (!url.length) return res.status(404).send({ message: "URL not found" });
+    const url = await process.openUrl(req.params.shortUrl);
+    if (!url) return res.status(404).send({ message: "URL not found" });
 
-    return res.redirect(302, url[0].url);
+    return res.redirect(302, url);
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
 };
 
 const deleteUrl = async (req, res) => {
-  const id = res.locals.user;
-  const { id: urlId } = req.params;
-  if (!urlId) return res.status(401).send({ message: "Missing url id" });
-
-  const query = "SELECT * FROM urls WHERE id = $1";
-  const queryDelete = "DELETE FROM urls WHERE id = $1";
+  const { id } = req.params;
+  if (!id) return res.status(401).send({ message: "Missing url id" });
 
   try {
-    const { rows: line } = await db.query(query, [urlId]);
-    if (!line.length) return res.status(404).send({ message: "URL not found" });
+    const line = await process.getUrl(id);
+    if (!line) return res.status(404).send({ message: "URL not found" });
 
-    if (line[0].userId !== parseInt(id)) return res.status(401).send({ message: "Unauthorized" });
+    if (line.userId !== parseInt(res.locals.user)) return res.status(401).send({ message: "Unauthorized" });
 
-    await db.query(queryDelete, [urlId]);
+    await process.deleteUrl(id);
+
     res.sendStatus(204);
   } catch (err) {
     res.status(500).send({ message: err.message });
--- src/repositories/processRepository.js
+++ src/repositories/processRepository.js
@@ -6,11 +6,53 @@ const registerUrl = async (id, url) => {
   const query = `INSERT INTO urls ("userId", url, "shortUrl") VALUES ($1, $2, $3) RETURNING *`;
 
   try {
-    const { rows: line } = await db.query(query, [id, url, shortUrl]);
-    if (!line.length) throw new Error("Error to insert url");
+    const {
+      rows: [line],
+    } = await db.query(query, [id, url, shortUrl]);
+    if (!line) throw new Error("Error to insert url");
 
-    return line[0];
+    return line;
   } catch (err) {
     throw new Error(err.message);
   }
 };
+
+const getUrl = async (id) => {
+  const query = `SELECT * FROM urls WHERE id = $1`;
+
+  try {
+    const {
+      rows: [line],
+    } = await db.query(query, [id]);
+
+    return line;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+const openUrl = async (shortUrl) => {
+  const query = `UPDATE urls SET "visitCount" = "visitCount" + 1 WHERE "shortUrl" = $1 RETURNING url`;
+
+  try {
+    const {
+      rows: [url],
+    } = await db.query(query, [shortUrl]);
+
+    return url.url;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+const deleteUrl = async (id) => {
+  const query = "DELETE FROM urls WHERE id = $1";
+
+  try {
+    await db.query(query, [id]);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+export default { registerUrl, getUrl, openUrl, deleteUrl };
